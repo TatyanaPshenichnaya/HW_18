cb9b43ab8b147f32e95d2c2ac41679dccba074db
Tatyana Pshenichnaya <tinitta@mail.ru>
2023-05-05 01:05:55 +0300

    bot added

---
+++ Project_18_bot/config.py
@@ -0,0 +1,13 @@
+
+# name Currency_ex_pr18_Bot,
+TOKEN = '6169115634:AAGX-8uQOuF3XeC5hrTNne3NSeNSIDs-MP8'
+
+keys = {'евро':'EUR',
+       'рубль':'RUB',
+       'доллар':'USD'}
+
+'''
+keys = {'биткоин':'BTC',
+       'эфириум':'ETH',
+       'доллар':'USD'}
+'''
